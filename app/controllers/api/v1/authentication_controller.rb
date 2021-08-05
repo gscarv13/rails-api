78abe93dd922bdcd14e8ae7a5eafbc9209751796
Gus <gsc13@tuta.io>
2021-08-05 18:33:57 -0300

    Add password to user

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,21 +1,31 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      class AuthenticationError < StandardError; end
+
       rescue_from ActionController::ParameterMissing, with: :paramenter_missing
+      rescue_from AuthenticationError, with: :handle_unauthenticated
 
       def create
-        params.require(:password)
+        raise AuthenticationError unless user.authenticate(params.require(:password))
 
-        user = User.find_by(username: params.require(:username))
         token = AuthenticationTokenService.call(user.id)
 
         render json: { token: token }, status: :created
+      end
+
+      def user
+        @user ||= User.find_by(username: params.require(:username))
       end
 
       private
 
       def paramenter_missing(error)
         render json: { error: error.original_message }, status: :unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        head :unauthorized
       end
     end
   end
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 describe 'Books API', type: :request do
   describe 'POST /authenticate' do
-    let(:user) { FactoryBot.create(:user, username: 'BookSeller') }
+    let(:user) { FactoryBot.create(:user, username: 'BookSeller', password: 'asdasdasdasd') }
     it 'authenticates the client' do
-      post '/api/v1/authenticate/', params: { username: user.username, password: 'asd654' }
+      post '/api/v1/authenticate/', params: { username: user.username, password: 'asdasdasdasd' }
 
       expect(response).to have_http_status(:created)
       expect(response_body).to eq(
         {
-          'token' => '123'
+          'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w'
         }
       )
     end
 
     it 'should return error when username is missing' do
-      post '/api/v1/authenticate/', params: { password: 'asd654' }
+      post '/api/v1/authenticate/', params: { password: 'asdasdasdasd' }
 
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response_body).to eq(
@@ -34,6 +34,12 @@ describe 'Books API', type: :request do
           'error' => 'param is missing or the value is empty: password'
         }
       )
+    end
+
+    it 'returns error when password is wrong' do
+      post '/api/v1/authenticate', params: { username: user.username, password: 'incorrect' }
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
