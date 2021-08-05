3c024c2bb2da884af4bad129c1efd19f6b9111e5
Gus <gsc13@tuta.io>
2021-08-05 19:57:39 -0300

    Require user authentication to add and delete book

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,10 @@
 module Api
   module V1
     class BooksController < ApplicationController
+      include ActionController::HttpAuthentication::Token
       MAX_PAGINATION_LIMIT = 100
+
+      before_action :authenticate_user, only: %i[create destroy]
 
       def index
         # limit: will separate the all results into 2 groups and get the first one
@@ -32,6 +35,14 @@ module Api
       end
 
       private
+
+      def authenticate_user
+        token, _options = token_and_options(request)
+        user_id = AuthenticationTokenService.decode(token)
+        User.find(user_id)
+      rescue ActiveRecord::RecordNotFound
+        render status: :unauthorized
+      end
 
       def limit
         [
